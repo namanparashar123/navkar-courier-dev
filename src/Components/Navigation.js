b0e70a5cf2285108c87b91e08b35ac1339d0a77c
namanparashar123 <naman5770@gmail.com>
2023-04-25 20:04:34 -0500

    Navbar styling

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import './Navigation.css';
+import "./Navigation.css";
 
 const Navigation = () => {
-    return <div className='navigation'>
-        <ul className='nav-bar'>
-            <li>Home</li>
-            <li>Compnay</li>
-            <ul>
-                <li>About Us</li>
-                <li>Careers</li>
-                <li>Locations</li>
-            </ul>
-            <li>Services</li>
-            <li>Contact Us</li>
+  return (
+    <div className="navigation">
+      <ul className="nav-bar">
+        <li>Home</li>
+        <li>Compnay</li>
+        <ul className="nav-bar_drop">
+          <li>About Us</li>
+          <li>Careers</li>
+          <li>Locations</li>
         </ul>
+        <li>Services</li>
+        <li>Contact Us</li>
+      </ul>
     </div>
-
-}
+  );
+};
 
 export default Navigation;

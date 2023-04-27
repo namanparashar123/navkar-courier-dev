a5b7cab7d7ab5c7dbf752b32b7089c53ebe9ce79
namanparashar123 <naman5770@gmail.com>
2023-04-27 07:20:52 -0500

    Installed react router and started the home page.

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+    return <div>
+        Content that is to be displayed at the home page.
+    </div>
+}
+
+export default Home;

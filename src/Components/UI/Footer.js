0665e0fc983ac70e067f6daeffa4e7bef70b36c0
Sahib4 <68962355+Sahib4@users.noreply.github.com>
2023-04-29 18:06:28 -0500

    Footer info

--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -7,22 +7,33 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer>
-      <div className="footerImg">
-        <a href="/">
+    <footer className="container-fluid sec5 footercol">
+      <div className="row">
+      <div className="col-sm-5 input">
+        <a href="/" >
           <img src={logo} alt="logo" />
         </a>
       </div>
 
-      <div>
+      <div className="col-sm-4 m-address">
         <h5>Mailing Address</h5>
         <p>Address: 50 Chenier Bay <br /> Winnipeg <br /> R3X1K2</p> 
         <p>Email: info@navkarcourier.ca</p>
         <p>Mobile No: +1(204)999-3472</p>
       </div>
-          <h5> Our Company</h5>
+      <div className="col-sm-3 links">
+          <h5 > Our Company</h5>
+            <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Contact Us</li>
+            </ul>
       <div>
-
+      </div>
+      </div>
+      <div className="footer">{`Copyright © Navkar Courier Ltd. ${year} | All Rights Reserved | Designed and developed by Naman Parashar`}
+      </div>
       </div>
     </footer>
   );

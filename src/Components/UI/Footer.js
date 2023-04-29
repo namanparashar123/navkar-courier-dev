bf5068a4b40600cd421eaaf9e926e8792814028f
namanparashar123 <naman5770@gmail.com>
2023-04-29 18:47:15 -0500

    Footer styling

--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -12,6 +12,7 @@ const Footer = () => {
       <div className="col-sm-5 input">
         <a href="/" >
           <img src={logo} alt="logo" />
+          <h1>Estd. 2020</h1>
         </a>
       </div>
 
@@ -23,7 +24,7 @@ const Footer = () => {
       </div>
       <div className="col-sm-3 links">
           <h5 > Our Company</h5>
-            <ul>
+            <ul className="footer_nav">
               <li><a href='/'>Home</a></li>
               <li><a href='/company'>Company </a></li>
               <li><a href='/Services'>Services</a></li>

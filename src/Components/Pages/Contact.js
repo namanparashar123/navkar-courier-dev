f23fe6d013e7a61a97270ef9be53aaa7a1386061
namanparashar123 <naman5770@gmail.com>
2023-05-04 17:48:00 -0500

    Completed the contact page layout.

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React from "react";
+
+import "./Contact.css";
 
 export default function Contact() {
   return (
     <div>
+      <h1 className="intro">Say hello!</h1>
+      <p className="contact_content">
+        Weather you are a customer, driver or a potential client, please reach out if you have any questions or feedback for us! We take feedback very seriously and are devoted to make the process easier for everybody.
+      </p>
+      <form className="mainForm" action="" onSubmit="">
+        <div className="formWord">
+          <input
+            className="input100"
+            type="text"
+            name="fullName"
+            placeholder="Full Name"
+            required
+          />
+          <br />
+          <input
+            className="input100"
+            type="tel"
+            name="phone"
+            placeholder="Phone Number"
+            required
+          />
+          <br />
+          <input
+            className="input100"
+            type="text"
+            name="email"
+            placeholder="Email"
+            required
+          />
+          <br />
+        </div>
+        <div className="formMessage">
+          <textarea
+            className="input100"
+            name="message"
+            placeholder="Your Message..."
+            required
+          ></textarea>
+          <br />
+          <button className="submitBtn">SUBMIT</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -9,32 +9,38 @@ const Footer = () => {
   return (
     <footer className="container-fluid sec5 footercol">
       <div className="row">
-      <div className="col-sm-5 input">
-        <a href="/" >
-          <img src={footerLogo} alt="logo" />
-          <h1>Estd. 2020</h1>
-        </a>
-      </div>
+        <div className="col-sm-5 input">
+          <a href="/">
+            <img src={footerLogo} alt="logo" />
+            <h1>Estd. 2020</h1>
+          </a>
+        </div>
 
-      <div className="col-sm-4 m-address">
-        <h5>Mailing Address</h5>
-        <p>Address: 50 Chenier Bay <br /> Winnipeg <br /> R3X1K2</p> 
-        <p>Email: info@navkarcourier.ca</p>
-        <p>Mobile No: +1(204)999-3472</p>
-      </div>
-      <div className="col-sm-3 links">
+        <div className="col-sm-4 m-address">
+          <h5>Mailing Address</h5>
+          <p>
+            Address: 50 Chenier Bay <br /> Winnipeg <br /> R3X1K2
+          </p>
+          <p>Email: info@navkarcourier.ca</p>
+          <p>Mobile No: +1(204)999-3472</p>
+        </div>
+        <div className="col-sm-3 links">
           <h5> Our Company</h5>
-            <ul className="footer_nav">
-              <li><a href='/'>Home</a></li>
-              <li><a href='/company'>Company </a></li>
-              <li><a href='/Services'>Services</a></li>
-              <li><a href='/contact'>Contact Us</a></li>
-            </ul>
-      <div>
-      </div>
-      </div>
-      <div className="footer line">{`Copyright © Navkar Courier Ltd. ${year} | All Rights Reserved | Designed and developed by Naman Parashar`}
-      </div>
+          <ul className="footer_nav">
+            <li>
+              <a href="/About">About Us</a>
+            </li>
+            <li>
+              <a href="/Careers">Careers</a>
+            </li>
+            <li>
+              <a href="/contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer line">
+          {`Copyright © Navkar Courier Ltd. ${year} | All Rights Reserved | Designed and developed by Naman Parashar`}
+        </div>
       </div>
     </footer>
   );

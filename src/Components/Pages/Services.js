cec1a3f073307b9f7a679920bc5e642c04e506d7
namanparashar123 <naman5770@gmail.com>
2023-05-04 18:40:25 -0500

    Started the image slider

--- src/Components/Pages/Services.js
+++ src/Components/Pages/Services.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import winnipeg from "../images/winnipeg2.png";
 
 export default function Services() {
   return (
     <div>
+      <img src={winnipeg} alt="Winnipeg"/>
     </div>
   )
 }

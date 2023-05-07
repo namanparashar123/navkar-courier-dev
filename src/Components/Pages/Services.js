6952481fd9c0bafed40eadb7aa7821b9951a49b8
namanparashar123 <naman5770@gmail.com>
2023-05-07 17:14:28 -0500

    Added some images

--- src/Components/Pages/Services.js
+++ src/Components/Pages/Services.js
@@ -1,10 +1,8 @@
-import React from 'react'
-import winnipeg from "../images/winnipeg2.png";
+import React from 'react';
 
 export default function Services() {
   return (
     <div>
-      <img src={winnipeg} alt="Winnipeg"/>
     </div>
   )
 }
